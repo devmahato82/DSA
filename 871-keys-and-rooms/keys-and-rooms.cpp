bb890d6b35ec0dffb14527bc4c9d3786f211622f
Dev Kumar Mahato <devmahato930@gmail.com>
2026-03-06 21:23:10 +0530

    Time: 4 ms (10.03%) | Memory: 14.9 MB (24.89%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> visited;
+        queue<int> q;
+        q.push(0);
+        visited.insert(0);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            for(int neighbour:rooms[curr] ) {
+                if(visited.find(neighbour) == visited.end()){
+                   q.push(neighbour);
+                   visited.insert(neighbour);
+                }
+            }
+        }
+        return visited.size() == rooms.size() ? true:false;
+    }
+};
