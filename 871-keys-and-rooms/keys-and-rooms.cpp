7111493fc6da1dc506f0b8890504af9d408f2982
Dev Kumar Mahato <devmahato930@gmail.com>
2026-03-06 21:44:38 +0530

    Time: 3 ms (16.76%) | Memory: 15 MB (14.30%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
+void dfs(vector<vector<int>>& rooms, unordered_set<int>& visited, int room){
+    if(visited.find(room) != visited.end()) return;
+    visited.insert(room);
+    for(int neighbour: rooms[room]){
+        if(visited.find(neighbour) == visited.end())
+            dfs(rooms,visited,neighbour);
+    }
+}
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //BFS
         unordered_set<int> visited;
-        queue<int> q;
-        q.push(0);
-        visited.insert(0);
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(int neighbour:rooms[curr] ) {
-                if(visited.find(neighbour) == visited.end()){
-                   q.push(neighbour);
-                   visited.insert(neighbour);
-                }
-            }
-        }
+        dfs(rooms,visited,0);
         return visited.size() == rooms.size() ? true:false;
     }
 };
