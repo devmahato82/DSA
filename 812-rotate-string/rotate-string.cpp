2e4898fb4d5238ad0d3638fbe20ec51de3a807f5
Dev Kumar Mahato <devmahato930@gmail.com>
2026-03-13 20:59:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (36.23%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n1 = s.size();
+        int n2 = goal.size();
+        if(n1!=n2) return false;
+        for(int i=0;i<n1;i++){
+            if(s==goal) return true;
+            char A = s[0];
+            s.erase(0,1);
+            s=s+A;
+        }
+        return false;
+    }
+};
