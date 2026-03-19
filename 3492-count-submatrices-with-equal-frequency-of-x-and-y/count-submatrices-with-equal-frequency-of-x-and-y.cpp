cf0323c46f74cbb6a01fbae7fc3e39bffdc05b45
Dev Kumar Mahato <devmahato930@gmail.com>
2026-03-20 00:16:19 +0530

    Time: 80 ms (21.95%) | Memory: 188.7 MB (6.50%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
+
+    vector<vector<int>> F(int n, int m, vector<vector<int>> a){
+
+        auto psum = a;
+
+        for(int i = 1; i < n; i++){
+            psum[i][0] = psum[i-1][0] + a[i][0];
+        }
+
+        for(int j = 1; j < m; j++){
+            psum[0][j] = psum[0][j-1] + a[0][j];
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                psum[i][j] = psum[i-1][j] + psum[i][j-1] + a[i][j] - psum[i-1][j-1];
+            }
+        }
+
+        return psum;
+
+    }
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> nums(n,vector<int>(m,0));
-        vector<vector<int>> x(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 'X'){
-                    nums[i][j] =1;
-                    x[i][j] = 1;
-                }
-                if(grid[i][j] == 'Y'){
-                    nums[i][j] = -1;
-                }
+        
+
+        int n = (int)grid.size();
+        int m = (int)grid[0].size();
+
+        vector<vector<int>> x(n, vector<int>(m, 0));
+        vector<vector<int>> y(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                x[i][j] = (grid[i][j] == 'X');
+                y[i][j] = (grid[i][j] == 'Y');
             }
         }
-        vector<vector<int>> preSumnums(n,vector<int>(m,0));
-        vector<vector<int>> preSumx(n,vector<int>(m,0));
-        preSumnums[0][0] = nums[0][0];
-        preSumx[0][0] = x[0][0];
 
-        for(int i=1;i<n;i++){
-            preSumnums[i][0]= preSumnums[i-1][0] + nums[i][0];
-            preSumx[i][0] = preSumx[i-1][0] + x[i][0];
-        }
-        for(int j=1;j<m;j++){
-            preSumnums[0][j]= preSumnums[0][j-1] + nums[0][j];
-            preSumx[0][j] = preSumx[0][j-1] + x[0][j];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                preSumnums[i][j] = nums[i][j] + preSumnums[i-1][j] + preSumnums[i][j-1] - preSumnums[i-1][j-1];
-                preSumx[i][j] = x[i][j] + preSumx[i-1][j] + preSumx[i][j-1] - preSumx[i-1][j-1];
+        auto px = F(n, m, x);
+        auto py = F(n, m, y);
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(px[i][j] == 0) continue;
+                if(px[i][j] == py[i][j]) ans++;
             }
         }
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(preSumnums[i][j]==0 && preSumx[i][j]>=1){
-                    count++;
-                }
-            }
-        }
-        return count;
 
-
+        return ans;
     }
 };
