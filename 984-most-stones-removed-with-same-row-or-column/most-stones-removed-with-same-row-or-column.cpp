50b1fa441f8e7e4ab45424bc310c26c49ebb2d5e
Dev Kumar Mahato <devmahato930@gmail.com>
2026-03-20 00:49:29 +0530

    Time: 2976 ms (5.02%) | Memory: 660.9 MB (5.04%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void dfs(int src, unordered_set<int> &vis,vector<vector<int>>graph){
+        vis.insert(src);
+        for(auto neighbour: graph[src]){
+            if(vis.find(neighbour) == vis.end()){
+                dfs(neighbour,vis,graph);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+
+        int n = stones.size();
+        vector<vector<int>> graph(n+1);
+        unordered_map<int, int> x, y;
+
+        for(int u = 1; u <= n; u++){
+            int px = stones[u-1][0];
+            int py = stones[u-1][1];
+
+            if(x.count(px)){
+                int v = x[px];
+                graph[u].push_back(v);
+                graph[v].push_back(u);
+            }
+
+            if(y.count(py)){
+                int v = y[py];
+                graph[u].push_back(v);
+                graph[v].push_back(u);
+            }
+
+            x[px] = u;
+            y[py] = u;
+        }
+        int cc=0;
+        unordered_set<int> vis;
+        for(int i=1;i<=n;i++){
+            if(vis.find(i)==vis.end()){
+                cc++;
+                dfs(i,vis,graph);
+            }
+        }
+        return n-cc;
+    }
+};
