017d7d5fa477ef1b4945745a45fd870e2cbbf43f
Dev Kumar Mahato <devmahato930@gmail.com>
2026-07-09 07:46:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
@@ -15,13 +15,22 @@ public:
 
 class Solution {
   public:
-    
     int height(Node* root) {
         // code here
-       if(root == NULL) return -1;
-       int lheight = height(root->left);
-       int rheight = height(root->right);
-       return max(lheight,rheight) +1;
+        queue<Node*> q;
+        q.push(root);
+        int depth =0;
+        while(q.size() != 0){
+            int levelsize = q.size();
+            for(int i=0; i< levelsize; i++) {
+                Node* curr = q.front();
+                q.pop();
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            depth++;
+        }
+        return depth -1;
         
     }
 };
