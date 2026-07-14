1c2a65fc208a29f267fa3355504de89db681c475
Dev Kumar Mahato <devmahato930@gmail.com>
2026-07-14 10:15:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (59.14%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        vector<vector<int>> dp(m,vector<int>(m));
+        for(int i=0; i<m; i++) {
+            dp[m-1][i] = triangle[m-1][i];
+        }
+
+        for(int i=m-2; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,diag);
+            }
+        }
+        return dp[0][0];
+    }
+};
