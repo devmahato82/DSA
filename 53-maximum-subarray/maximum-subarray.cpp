ca7b6602989b1ce6cf40a8770d5a75ab7a6af6a6
Dev Kumar Mahato <devmahato930@gmail.com>
2026-07-16 00:02:13 +0530

    Time: 3 ms (19.12%) | Memory: 71.8 MB (52.57%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,8 +5,8 @@ public:
         int maxi = INT_MIN;
         int sum =0;
         for(int i=0; i<n; i++) {
-            sum += nums[i];
-            if(sum > maxi) {
+            sum+= nums[i];
+            if(sum> maxi) {
                 maxi = sum;
             }
             if(sum<0){
