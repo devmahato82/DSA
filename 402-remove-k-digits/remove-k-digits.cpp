5124c8286785e4531b2b5ad5e0f5389bf1ec1142
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-01 01:44:08 +0530

    Time: 7 ms (24.63%) | Memory: 11.3 MB (65.21%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -4,7 +4,7 @@ public:
         int n = num.size();
         stack<char> st;
         for(int i=0; i<n; i++) {
-            while(!st.empty() && k>0 && (st.top()-'a') > (num[i]-'a')) {
+            while(!st.empty() && k>0 && (st.top()-'0') > (num[i]-'0')) {
                 st.pop();
                 k--;
             }
