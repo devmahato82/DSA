c74a3ab981c456b7b1e3ed0a6a19b516537e7ea1
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-20 23:41:56 +0530

    Time: 140 ms (44.13%) | Memory: 17.5 MB (55.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -18,7 +18,23 @@ public:
             sum+=nums[i];
         }
         if(sum%2 !=0) return false;
-        vector<vector<int>> dp(n, vector<int>(sum/2 +1,-1));
-        return helper(n-1, sum/2, nums, dp);
+        vector<vector<bool>> dp(n, vector<bool>(sum/2 +1,false));
+        //return helper(n-1, sum/2, nums, dp);
+
+        for(int i=0; i<n; i++) dp[i][0] = true;
+        if(nums[0]<= sum/2) dp[0][nums[0]] =  true;
+        for(int ind =1; ind<n; ind++) {
+            for(int target=1; target<= sum/2; target++) {
+                bool nottake = dp[ind-1][target];
+                bool take = false;
+                if(nums[ind]<= target) {
+                    take = dp[ind-1][target-nums[ind]];
+                } 
+                dp[ind][target] = take | nottake;
+            }
+        }
+        return dp[n-1][sum/2];
+
+
     }
 };
