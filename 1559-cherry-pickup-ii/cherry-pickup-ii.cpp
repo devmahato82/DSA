44eb1471ee0cc87a036a3aa883a6abdf906f6d49
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-20 15:49:37 +0530

    Time: 40 ms (76.62%) | Memory: 20.9 MB (74.98%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int helper(int i,int j1, int j2,int m,int n, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp) {
+        if(i==m-1) {
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+        if(j1<0 || j1>=n || j2<0 || j2>=n) return -1e9;
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = -1e9;
+        for(int dj1=-1; dj1<=1; dj1++) {
+            for(int dj2=-1; dj2<=1; dj2++) {
+                if(j1+dj1 <0 || j2+dj2<0 || j1+dj1>=n || j2+dj2>=n) continue;
+                int val=0;
+                if(j1==j2) {
+                    val = grid[i][j1];
+                }
+                else val = grid[i][j1] + grid[i][j2];
+                val += helper(i+1, j1+dj1, j2+dj2,m, n, grid, dp);
+                maxi = max(maxi, val);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return helper(0,0,n-1,m,n,grid, dp);
+
+    }
+};
