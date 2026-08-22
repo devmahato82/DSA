593491f9bc155edf2b8ce3cd7ed15392c79a297a
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-22 15:35:19 +0530

    Time: 27 ms (66.96%) | Memory: 36.7 MB (49.96%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int helper(int ind, int amount, vector<int>& coins, vector<vector<int>> &dp) {
+        if(ind==0) {
+            if(amount%coins[0]==0 || amount ==0) return 1;
+            else return 0;
+        }
+        if(amount == 0) return 1;
+        if(amount<0) return 0;
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        int nottake = helper(ind-1, amount, coins, dp);
+        int take =0;
+        if(coins[ind] <= amount) take = helper(ind, amount-coins[ind],coins, dp);
+        return dp[ind][amount] = take+nottake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n =  coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        return helper(n-1, amount, coins, dp);
+    }
+};
