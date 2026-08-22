16f514559f86d05f19a08024fe9ab8a6ea754539
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-23 00:32:19 +0530

    Time: 23 ms (82.14%) | Memory: 27.5 MB (55.62%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+    int helper(int ind1, int ind2, string text1, string text2, vector<vector<int>>& dp) {
+        if(ind1 < 0 || ind2 < 0) return 0;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(text1[ind1] == text2[ind2]) return dp[ind1][ind2] = 1 + helper(ind1-1, ind2-1, text1, text2,dp);
+        else return dp[ind1][ind2] = max(helper(ind1, ind2-1, text1, text2, dp),helper(ind1-1, ind2, text1, text2, dp));
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1,0));
+        //return helper(n1-1, n2-1, text1, text2,dp);
 
-        // Create DP table to store lengths of LCS for all substrings
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        // Fill dp table bottom-up
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    // Characters match: increase length by 1
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // Characters don't match: take max of left and top
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        for(int ind1 = 1; ind1<=n1; ind1++) {
+            for(int ind2=1; ind2<=n2; ind2++) {
+                if(text1[ind1-1] == text2[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] = max(dp[ind1][ind2-1],dp[ind1-1][ind2]);
             }
         }
-
-        
-        return dp[n][m];
+        return dp[n1][n2];
     }
 };
