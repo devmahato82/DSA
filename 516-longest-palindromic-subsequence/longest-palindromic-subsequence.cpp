a9da70295a54ec5ba4a69059c5813b2d7552be08
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-23 17:36:34 +0530

    Time: 91 ms (33.27%) | Memory: 76 MB (22.89%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int helper(int ind1, int ind2, string s, string rev, vector<vector<int>> &dp) {
+        if(ind1<0 || ind2<0) return 0;
+ 
+        if(dp[ind1][ind2] !=-1) return dp[ind1][ind2];
+        if(s[ind1] == rev[ind2]) return dp[ind1][ind2] = 1+helper(ind1-1, ind2-1, s, rev,dp);
+        return dp[ind1][ind2] =  max(helper(ind1, ind2-1, s, rev, dp), helper(ind1-1, ind2, s, rev, dp));
+    }
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        //return helper(n-1, n-1,s,rev,dp);
+
+        for(int ind1=1; ind1<=n; ind1++){
+            for(int ind2=1; ind2<=n; ind2++) {
+                if(s[ind1-1] == rev[ind2-1]) dp[ind1][ind2] = 1+ dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] =  max(dp[ind1][ind2-1], dp[ind1-1][ind2] );
+            }
+        }
+        return dp[n][n];
+    }
+};
