c4fb83f00d6c2162224a1a33c462190d86f277f4
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-25 16:18:19 +0530

    Time: 25 ms (71.80%) | Memory: 18.3 MB (38.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int cc=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j] == '0'){
-                    continue;
-                }
-                cc++;
-                grid[i][j] = '0';
-                queue<pair<int,int>> q;
-                q.push({i,j});
-                while(q.size()>0){
-                    auto curr = q.front();
-                    q.pop();
-                    int currRow = curr.first;
-                    int currCol = curr.second;
-                    if(currRow>0 && grid[currRow-1][currCol] == '1'){
-                        q.push({currRow-1,currCol});
-                        grid[currRow-1][currCol] = '0';
-                    }
-                    if(currRow<rows-1 && grid[currRow+1][currCol] == '1'){
-                        q.push({currRow+1,currCol});
-                        grid[currRow+1][currCol] = '0';
-                    }
-                    if(currCol>0 && grid[currRow][currCol-1] == '1'){
-                        q.push({currRow,currCol-1});
-                        grid[currRow][currCol-1] = '0';
-                    }
-                    if(currCol<cols-1 && grid[currRow][currCol+1] == '1'){
-                        q.push({currRow,currCol+1});
-                        grid[currRow][currCol+1] = '0';
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        int count =0;
+        queue<pair<int,int>> q;
+        for(int i=0; i<m; i++) {
+            for(int j=0;j<n; j++) {
+                if(grid[i][j] == '1'){
+                    count++;
+                    grid[i][j] = '0';
+                    q.push({i,j});
+                    while(!q.empty()) {
+                        int r = q.front().first;
+                        int c = q.front().second;
+                        q.pop();
+                        for(int i=0;i<4; i++) {
+                            int nrow = r + drow[i];
+                            int ncol = c + dcol[i];
+                            if(nrow>=0 && nrow<m && ncol>=0 && ncol< n && grid[nrow][ncol]=='1') {
+                                q.push({nrow,ncol});
+                                grid[nrow][ncol] = '0';
+                            }
+                        }
+
                     }
                 }
-                
             }
         }
-        return cc;
+        return count;
     }
 };
