5e5d73f3f36b6f15403cb55d4e2e2af4a841601e
Dev Kumar Mahato <devmahato930@gmail.com>
2026-08-29 23:30:43 +0530

    Time: 1899 ms (10.64%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -4,6 +4,6 @@ from queue q1 inner join queue q2
 on q1.turn >= q2.turn
 group by q1.turn, q1.person_name
 having sum(q2.weight) <= 1000
-ORDER BY q1.turn DESC
+ORDER BY sum(q2.weight) DESC
 limit 1
 
