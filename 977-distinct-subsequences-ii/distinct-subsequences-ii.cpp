512f6fd381b5eba338b7bd249781063ebddb6779
Dev Kumar Mahato <devmahato930@gmail.com>
2026-09-07 19:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (78.81%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int distinctSubseqII(string s) {
-        const int MOD = 1e9 + 7;
+        int MOD = 1e9 + 7;
         int n = s.size();
 
-        long long total = 1; // dp[0] = 1 (empty subsequence)
-        vector<int> last(26, -1); // last occurrence of each char
+        long long total = 1; 
+        vector<int> last(26, -1); 
 
         for(int i = 0; i < n; i++) {
             long long newTotal = (2 * total) % MOD;
 
             int ch = s[i] - 'a';
             if(last[ch] != -1) {
-                // subtract subsequences counted before last occurrence
                 newTotal = (newTotal - last[ch] + MOD) % MOD;
             }
 
-            // update last[ch] to store the contribution of subsequences
             last[ch] = total;
 
             total = newTotal;
         }
 
-        return (total - 1 + MOD) % MOD; // exclude empty subsequence
+        return (total - 1 + MOD) % MOD;
     }
 };
