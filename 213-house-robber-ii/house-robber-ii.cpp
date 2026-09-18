e43a3f0c6b21a1b868f8bbd7d39ce470579e1994
Dev Kumar Mahato <devmahato930@gmail.com>
2026-09-18 18:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (37.84%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -12,10 +12,24 @@ public:
         int n = nums.size();
         if(n==1) return nums[0];
         if(n==2) return max(nums[0], nums[1]);
-        vector<int> dp1(n+2,-1);
-        vector<int> dp2(n+2,-1);
-       // if(n%2==0) 
-        return max(find(0,n-1,nums,dp1), find(1,n,nums,dp2));
-        //return max(max(nums[0],nums[n-1])+find(2,n-1,nums,dp1), find(1,n,nums,dp2));
+        // vector<int> dp1(n+2,-1);
+        // vector<int> dp2(n+2,-1);
+        // return max(find(0,n-1,nums,dp1), find(1,n,nums,dp2));
+
+
+        vector<int>dp1(n+1), dp2(n+2);
+
+        dp1[n] = dp1[n-1] = 0;
+        for(int i= n-2;i>=0;i--) {
+            dp1[i] = max(nums[i]+dp1[i+2], dp1[i+1]);
+        }
+
+        dp2[n] = dp2[n+1] =0;
+        for(int i=n-1; i>=1; i--) {
+            dp2[i] = max(nums[i]+dp2[i+2], dp2[i+1]);
+        }
+        return max(dp1[0],dp2[1]);
+
+    
     }
 };
