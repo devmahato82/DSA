291b483eb10d1064d9ca7207c85d5607a9f70ddb
Dev Kumar Mahato <devmahato930@gmail.com>
2026-09-18 20:04:43 +0530

    Time: 59 ms (15.72%) | Memory: 82.1 MB (5.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
-    long long f(int ind, int amount,int n, vector<int>& coins, vector<vector<long long>>& dp) {
-        if(ind == n) {
-            if(amount%coins[ind] == 0) return amount/coins[ind];
+    long long f(int n, int amount, vector<int>& coins, vector<vector<long long>>& dp) {
+        if(n==0) {
+            if(amount%coins[n] == 0) return amount/coins[0];
             else return INT_MAX;
         }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        long long nottake = f(ind+1,amount,n,coins,dp);
+
+        if(dp[n][amount] != -1) return dp[n][amount];
+        
+        long long nottake = f(n-1, amount, coins, dp);
         long long take = INT_MAX;
-        if(amount>= coins[ind]) {
-            take = 1 + f(ind,amount-coins[ind], n, coins, dp);
-        }
-        return dp[ind][amount] = min(take, nottake);
+        if(amount>= coins[n]) 
+            take = 1 + f(n, amount-coins[n], coins, dp);
+        return dp[n][amount] = min(nottake, take);
+
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
+        vector<vector<long long>> dp(n, vector<long long>(amount+1,-1));
 
-        vector<vector<long long>> dp(n, vector<long long>(amount + 1, INT_MAX));
-
-        for(int i = 0; i < n; i++)
-            dp[i][0] = 0;
-
-        for(int j = 0; j <= amount; j++) {
-            if(j % coins[n-1] == 0)
-                dp[n-1][j] = j / coins[n-1];
-        }
-
-        for(int i = n - 2; i >= 0; i--) {
-            for(int j = 0; j <= amount; j++) {
-
-                long long nottake = dp[i+1][j];
-
-                long long take = INT_MAX;
-                if(j >= coins[i] && dp[i][j - coins[i]] != INT_MAX) {
-                    take = 1 + dp[i][j - coins[i]];
-                }
-
-                dp[i][j] = min(take, nottake);
-            }
-        }
-
-        return dp[0][amount] == INT_MAX ? -1 : dp[0][amount];
+        long long ans = f(n-1, amount, coins, dp);
+        return ans==INT_MAX ? -1:ans;
     }
-
 };
